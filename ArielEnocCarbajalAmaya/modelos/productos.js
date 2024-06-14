788163bc5c8e70761e1256abdde28857a7648dd6
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-06-14 15:23:58 -0600

    Api de Ariel Carbajal

---
+++ ArielEnocCarbajalAmaya/modelos/productos.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Productoschema = new mongoose.Schema({
+    producto:{
+        type: String,
+        require: true
+    },
+    categoria:{
+        type: String,
+        require: true
+    },
+    existencia:{
+        type: Number,
+        require: true
+    },
+    precio:{
+        type: Number,
+        require: true
+    },
+    imagen:{
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('Producto', Productoschema)
