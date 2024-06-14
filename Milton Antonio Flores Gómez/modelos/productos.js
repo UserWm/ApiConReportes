06100e3564a6a315e679a475e201ad48a3eaed6b
Milton2003AFG <miltonflores2003@gmail.com>
2024-06-14 15:19:20 -0600

    Api Reportes
    Api

---
+++ Milton Antonio Flores Gómez/modelos/productos.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const ProductoSchema = new mongoose.Schema({
+    producto:{
+        type: String,
+        require: true
+    },
+    categoria:{
+        type: String,
+        require: true
+    },
+    existencia:{
+        type: Number,
+        require: true
+    },
+    precio:{
+        type: Number,
+        require: true
+    },
+    imagen:{
+        type: String,
+        require: true
+    }
+})
+
+module.exports = mongoose.model('Producto', ProductoSchema)
+
