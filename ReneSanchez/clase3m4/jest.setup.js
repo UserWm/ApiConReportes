9fa691feab86d8072a23d400904cc53875c7079f
Rasp3600 <127553473+Rasp3600@users.noreply.github.com>
2024-06-14 16:40:20 -0600

    API Rene Sanchez

---
+++ ReneSanchez/clase3m4/jest.setup.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+beforeAll(async () => {
+  const url = "mongodb://localhost:27017/testUnit";
+  mongoose
+    .connect(url)
+    .then(() => console.log("Mongo De prueba is Ready PAPAAAAAAAAAAA"))
+    .catch((err) => console.log(err));
+});
+
+afterAll(async () => {
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.connection.close();
+    console.log("Conexion a MongoDB cerrada");
+    await mongoose.connection.db.dropDatabase();
+  }
+});
